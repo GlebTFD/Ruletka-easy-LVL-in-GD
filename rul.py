2bac3781459969f2e3488cd736a7c1339074d1b2
GlebTFD <glebiv2011@gmail.com>
2022-09-29 14:07:21 +0300

    init repo

---
+++ rul.py
@@ -0,0 +1,26 @@
+print("\033c")
+from random import *
+
+
+print("LVL:\nEasy\n6\nNormal\n4\nHard\n1")
+UrandomU = randint(1, 10)
+if UrandomU == 1:
+    print("Sonar")
+elif UrandomU == 2:
+    print("Retrey")
+elif UrandomU == 3:
+    print("Dark Paradise")
+elif UrandomU == 4:
+    print("Promises")
+elif UrandomU == 5:
+    print("Warmth")
+elif UrandomU == 6:
+    print("Ocean of Dreams")
+elif UrandomU == 7:
+    print("End of Line")
+elif UrandomU == 8:
+    print("Through Time")
+elif UrandomU == 9:
+    print("Glitter Mannes 2k15")
+elif UrandomU == 10:
+    print("Almighty")
